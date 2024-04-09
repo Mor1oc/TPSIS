e3d65253e5d9610fc25154c0276dcbec9472a2a0
Mor1oc <vadimgolushko2004@gmail.com>
2024-04-09 09:40:56 +0300

    4

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def autorization():
+    return FileResponse("application/templates/authorization.html")
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/orders")
+async def orders():
+    return FileResponse("application/templates/orders.html")
+
+@app.get("/warehouse")
+async def warehouse():
+    return FileResponse("application/templates/warehouse.html")
+
+@app.get("/carrier")
+async def carrier():
+    return FileResponse("application/templates/carrier.html")
+
+@app.get("/rout")
+async def rout():
+    return FileResponse("application/templates/rout.html")
+
+@app.get("/analysis")
+async def analysis():
+    return FileResponse("application/templates/analysis.html")
